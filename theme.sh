d42b27bb160b7c701d29167a0bc2771bfb261b77
Abel David <abel.david@ignitarium.com>
2019-08-05 17:28:46 +0530

    mac icon theme added

--- theme.sh
+++ theme.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo apt-get install gnome-tweak-tool
+
+
+sudo apt-get install unity-tweak-tool
+
+sudo add-apt-repository ppa:noobslab/macbuntu
+sudo apt-get update
+sudo apt-get install macbuntu-os-icons-lts-v7
+
