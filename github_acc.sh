c9fe8c9b7b98f1b568920551dbb2cfa71949119f
Abel David <abel.david@ignitarium.com>
2019-08-05 15:17:00 +0530

    init only

--- github_acc.sh
+++ github_acc.sh
@@ -3,5 +3,5 @@
 git config --global user.name "Abel David"
 git config --global user.email "abel.david@ignitarium.com"
 
-
+#By default, Git will cache your password for 15 minutes.
  git config --global credential.helper cache
