c8b77d0b61aa9e871e1fe8fcb735edb5d6c13d6a
Abel David <abel.david@ignitarium.com>
2019-08-05 15:10:18 +0530

    Vscode exts added.

--- VS_Code.sh
+++ VS_Code.sh
@@ -8,5 +8,27 @@ wget -O vscode https://go.microsoft.com/fwlink/?LinkID=760868
 sudo dpkg -i /home/abeldavid/Downloads/zip/vscode && apt-get install -f
 
 
+declare -a extensions=(
+        donjayamanne.githistory # Git History
+        mhutchie.git-graph # Git Graph
+        alefragnani.project-manager # Project Manager
+        aaron-bond.better-comments # Better Comments
+        pkief.material-icon-theme # Material Theme Icon
+        equinusocio.vsc-material-theme # Material Theme
+        hoovercj.vscode-power-mode # Power Mode
+
+)
+
+for extension in "${extensions[@]}"; do
+    set +e
+    # Attempt to install extension; log message on success, log warning on failure
+    code --install-extension "$extension" &> /dev/null && \
+    set -e
+done
+
+echo "DONE.. Please Restart VS Code"
+echo "Ctrl + Shift + P into  User Settings to activate"
+
+
 
 
