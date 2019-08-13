61cc7a2b8499a2c2e9a93d9204246497d5f5b41c
Abel David <abel.david@ignitarium.com>
2019-08-13 16:14:45 +0530

    updated with new anaconda version

--- anaconda.sh
+++ anaconda.sh
@@ -6,16 +6,19 @@ sudo apt-get install curl
 
 cd /tmp
 
-curl -O https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 
 
-sha256sum Anaconda3-4.3.1-Linux-x86_64.sh
+sha256sum Anaconda3-5.0.1-Linux-x86_64.sh
 
 
-bash Anaconda3-4.3.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh
 
 
 source ~/.bashrc
 
 
 conda list
+
+
+conda install -c anaconda anaconda-navigator
