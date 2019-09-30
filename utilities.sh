80082ec4bb0d03e2692b5b8379f7b375ff4438c1
abelvdavid <52195886+abelvdavid@users.noreply.github.com>
2019-09-30 16:56:27 +0530

    enable win key

--- utilities.sh
+++ utilities.sh
@@ -8,3 +8,7 @@ sudo apt-get install brightness-controller
 # uninstall
 # sudo apt-get remove brightness-controller && sudo apt-get autoremove
 
+# Enable Windows Key in Ubuntu
+
+sudo apt-get remove dell-super-key 
+sudo apt-get install compizconfig-settings-manager -y
