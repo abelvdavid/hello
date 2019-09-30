7b34c594d56804fd640bd2a6427ed5ed81139916
abelvdavid <52195886+abelvdavid@users.noreply.github.com>
2019-09-30 15:02:23 +0530

    added brightness controller

---
+++ utilities.sh
@@ -0,0 +1,10 @@
+
+# Brightness Controller using Personal Package Archives
+
+sudo add-apt-repository ppa:apandada1/brightness-controller
+sudo apt-get update
+sudo apt-get install brightness-controller
+
+# uninstall
+# sudo apt-get remove brightness-controller && sudo apt-get autoremove
+
